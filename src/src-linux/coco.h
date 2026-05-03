9b608e81a9c4bd04973558bc957b6b64c5da73a9
06712L <06712LH@gmail.com>
2026-05-03 13:03:41 +0800

    V0.1-release

--- src/src-linux/coco.h
+++ src/src-linux/coco.h
@@ -5,6 +5,7 @@
 #define play_click system("aplay ./sound/click.wav > ./log.log 2>&1 &")
 #define play_tied system("aplay ./sound/tied.wav > ./log.log 2>&1 &")
 #define play_win system("aplay ./sound/winner.wav > ./log.log 2>&1 &")
+
 extern char *version;
 
 #endif
--- src/src-win/coco.h
+++ src/src-win/coco.h
@@ -5,6 +5,7 @@
 #define play_click PlaySound(TEXT(".\\sound\\click.wav"), NULL, SND_FILENAME | SND_ASYNC);
 #define play_tied PlaySound(TEXT(".\\sound\\tied.wav"), NULL, SND_FILENAME | SND_ASYNC);
 #define play_win PlaySound(TEXT(".\\sound\\win.wav"), NULL, SND_FILENAME | SND_ASYNC);
+
 extern char *version;
 
 #endif
